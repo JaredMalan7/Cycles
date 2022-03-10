d6130310e10c682964d7e4818549088a0dec6d09
Jared Malan <94416292+JaredMalan7@users.noreply.github.com>
2022-03-10 01:00:34 -0700

    Update move_actors_action.py
    fixed snake actions bug

--- game/scripting/move_actors_action.py
+++ game/scripting/move_actors_action.py
@@ -4,8 +4,10 @@ from game.scripting.action import Action
 class MoveActorsAction(Action):
 
     def execute(self, cast, script):
+        all_actors = cast.get_all_actors()
+        
         # 2) loop through the actors
 
-        for actor in cast.get_all_actors():
+        for snake in (all_actors):
             # 3) call the move_next() method on each actor
-            actor.move_next()
+            sanke.move_next()
